c86b704f205bee83c0290e7fe8ffb8f56a6a8482
Arthur Bonini <arthur.bonini@prs-invivo.com>
2020-07-27 21:05:38 -0400

    fix typos

--- server/index.js
+++ server/index.js
@@ -21,9 +21,11 @@ const pgClient = new Pool({
 });
 
 pgClient.on('error', () => console.log('lost pg connection') );
-pgClient
-    .query('CREATE TABLE IF NOT EXISTS values(number INT)')
-    .catch((err) => console.log(err));
+pgClient.on('connect', () => {
+    pgClient
+      .query('CREATE TABLE IF NOT EXISTS values (number INT)')
+      .catch((err) => console.log(err));
+  });
 
 
 
